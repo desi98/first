84d4c65f81a490ae93bb78895a767e49ca57bdb4
sachi_desilva <sachinidishala1998@gmail.com>
2022-02-28 18:13:39 +0530

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         String name = JOptionPane.showInputDialog("Hi! what is your name");
+        JOptionPane.showMessageDialog(null ,"There's a one thing I want to say you" + "\t"+name);
         JOptionPane.showMessageDialog(null ,"I Love you ! " +name);
 
 

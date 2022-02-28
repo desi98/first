03f6055f3a7e64e397f2ad9c7f911e59de9df3b5
sachi_desilva <sachinidishala1998@gmail.com>
2022-02-28 17:39:28 +0530

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Hi! what is your name");
+        JOptionPane.showMessageDialog(null ,"I Love you ! " +name);
+
+
+    }
+}
